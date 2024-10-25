af6ff685fd31ea31a9c5792d7a820af14343a511
Lester Bravo <ld.bravo@outlook.com>
2024-10-25 15:33:28 -0600

    Finished navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,28 @@
-import { Stack } from "expo-router";
+// import { Stack } from "expo-router";
+
+// export default function RootLayout() {
+//   return (
+//     <Stack>
+//       <Stack.Screen name="index" options={{title: "Home"}} />
+//       <Stack.Screen name="about" options={{title: "About"}} />
+//     </Stack>
+//   );
+// }
+
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{title: "Home"}} />
-      <Stack.Screen name="about" options={{title: "About"}} />
+
+      {/* @tutinfo This is how a tab navigator is nested inside a stack navigator,
+      especially when the Root layout is composed of a parent stack navigator.
+      We're also setting the headerShown option to false
+      to hide the header for the tab navigator. Otherwise, there will be two
+      headers displayed on each screen. */}
+
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
